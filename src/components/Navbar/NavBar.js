617c604f642578926eff4b20583a12a18595ea55
Daniel <valdez.daniel77@gmail.com>
2023-02-09 15:55:16 -0300

    correccion bug href

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -34,20 +34,20 @@ function NavBar() {
       <button onClick={(e) => {handleAllCharacter(e)}}>{boolean ? "Borrar todos los personajes" : "Mostrar todos los personajes"}</button>
 
       <SearchBar/> */}
-      <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
         <div class="container-fluid d-flex">
-          <a href="javascript:location.reload()"><button type="button" class="btn btn-info">Recargar</button></a>
+          <a href="#navbar"><button type="button" class="btn btn-info" onClick={() => window.location.reload()}>Recargar</button></a>
           {/* <a href="javascript:location.reload()"><img src="https://cdn-icons-png.flaticon.com/512/1539/1539194.png" alt="icon" width="40px"/></a> */}
             <div class="collapse navbar-collapse " id="navbarNav">
               <ul class="navbar-nav ">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#"><button type="button" class="btn btn-primary" onClick={(e) => {handleRandom(e)}}>Random</button></a>
+                  <a class="nav-link active" aria-current="page" href="#navbar"><button type="button" class="btn btn-primary" onClick={(e) => {handleRandom(e)}}>Random</button></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#"><button type="button" class="btn btn-success" onClick={(e) => {handleAllCharacter(e)}}>{boolean ? "Borrar todos los personajes" : "Mostrar todos los personajes"}</button></a>
+                  <a class="nav-link" href="#navbar"><button type="button" class="btn btn-success" onClick={(e) => {handleAllCharacter(e)}}>{boolean ? "Borrar todos los personajes" : "Mostrar todos los personajes"}</button></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link"><SearchBar/></a>
+                  <a href="#navbar" class="nav-link"><SearchBar/></a>
                 </li>
               </ul>
             </div>
